ff48a855cb93db49f2b3546a9b758388c008c7da
Dante Tam <dante.tam1@gmail.com>
2018-01-15 23:01:55 -0800

    Quest stage start/end progression + prepare to impl. callbacks

--- client/js/quest.js
+++ client/js/quest.js
@@ -1,5 +1,5 @@
 
-define(['character', 'items'], function(Character, Items) {
+define(['player', 'items'], function(Player, Items) {
 
     var Quest = Class.extend({
         init: function(name, stages, startingStage, listEndingStages) {
@@ -19,10 +19,77 @@ define(['character', 'items'], function(Character, Items) {
             this.player = player;
         },
 
-        objectiveCallback: function(stage) {
-            //TODO: Create a callback that activates upon completing the objective,
+        /**
+        Return the current set(s) of objectives, indexed by
+        next stage id -> array of objectives, all of which must be completed within this stage.
+        */
+        getCurrentObjectives: function() {
+            if (this.currentStagePointer != null) {
+                return this.stages[this.currentStagePointer]["stageTargets"];
+            }
+        },
+
+        /*objectiveCallback: function(objectiveJsonData, objCallback) {
+            //TODO: Create a set of callbacks that activate upon completing the objective,
             //executes the stageEnter/stageExit events,
             //and moves to the correct next stage.
+            return function() {
+
+            };
+        },*/
+
+        /**
+        The encapsulated method for advancing from the current stage (this.currentStagePointer)
+        to another stage, ideally a stage target.
+
+        This evaluates many possible callbacks:
+
+        upon leaving stage t, call t::onStageEnd();
+        //removed from quests JSON -> upon moving to stage t -> t+1, call t["target"]::uniqueStageEffect();
+        upon moving into stage t+1 officially, call (t+1)::onStageStart().
+        */
+        advanceToStageFromCur: function(nextStage) {
+            if (this.currentStagePointer != null) {
+                var stage = this.stages[this.currentStagePointer];
+
+                if (stage != null && stage.hasOwnProperty("onStageEnd")) {
+                    var stageEndData = stage["onStageEnd"];
+                    if (stageEndData !== "") {
+                        var callback = parseActionEffect(stageEndData);
+                        callback();
+                    }
+                }
+            }
+
+            this.currentStagePointer = "" + nextStage;
+
+            var stage = this.stages[this.currentStagePointer];
+
+            if (stage != null && stage.hasOwnProperty("onStageStart")) {
+                var stageStart = stage["onStageStart"];
+                if (stageEndData !== "") {
+                    var callback = parseActionEffect(stageStart);
+                    callback();
+                }
+            }
+        },
+
+        //Return callback for a jsonData action/effect intended to be used for stagestart/end
+        parseActionEffect: function(jsonData) {
+            if (jsonData === "") return null;
+            var action = jsonData["action"];
+            if (action === "teleport") {
+                return function() {
+
+                };
+            }
+            else if (action === "give") {
+                return function() {
+
+                };
+            }
+            log.error("Invalid action in quests JSON: " + jsonData);
+            return null;
         }
 
     });
